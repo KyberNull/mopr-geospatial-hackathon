ac268deb83039137b87fe3e467b9717b2532c57e
Aasish <50045306+KyberNull@users.noreply.github.com>
2026-04-15 13:49:40 +0000

    Update folder locations

--- config/eval.py
+++ config/eval.py
@@ -4,5 +4,5 @@ NUM_CLASSES_EVAL = 4
 NUM_BATCHES_EVAL = 8
 MAX_EXAMPLES_EVAL = 5
 IGNORE_LABEL = 255
-INPUT_DIR = "data/input_demo"
-MASK_DIR = "data/masks_demo"
+INPUT_DIR = "data/train/testing_dataset/processed_datasets"
+MASK_DIR = "data/train/testing_dataset/processed_masks"
